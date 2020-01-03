71cfb6e63c50d5181e3d3d2c4a664978919ecbc6
Rolando Lopantzi <rlopantzi@gmail.com>
2020-01-03 15:13:22 -0800

    Create js_objects.js

---
+++ JavaScript/js_objects.js
@@ -0,0 +1,16 @@
+var students = [
+    {name: 'Remy', cohort: 'Jan'},
+    {name: 'Genevieve', cohort: 'March'},
+    {name: 'Chuck', cohort: 'Jan'},
+    {name: 'Osmund', cohort: 'June'},
+    {name: 'Nikki', cohort: 'June'},
+    {name: 'Boris', cohort: 'June'}
+];
+
+function print(arr){
+    for(let i = 0; i < arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+
+print(students);
