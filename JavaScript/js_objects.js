3a89208485aacdff718b66c7c16cae95231f6b97
Rolando Lopantzi <rlopantzi@gmail.com>
2020-01-03 19:36:45 -0800

    init commit

--- JavaScript/js_objects.js
+++ JavaScript/js_objects.js
@@ -14,3 +14,26 @@ function print(arr){
 }
 
 print(students);
+
+var users = {
+    employees: [
+        {'first_name': 'Miguel', 'last_name' : 'Jones'},
+        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+    ],
+    managers: [
+        {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+        {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+    ]
+};
+
+function allUsers(obj){
+    for(let x in users){
+        console.log(x.toUpperCase());
+        for(let i = 0; i < users[x].length; i++){
+            let charCount = users[x].first_name.length + users[x].last_name.length;
+            console.log(i+ 1 + ' - ' + users[x][i].first_name.toUpperCase() + ' , ' + users[x][i].last_name.toUpperCase() + ' - ' + charCount);
+        }
+    }
+}
