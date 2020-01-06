627d06361e0e3190effe8723d9882fcbdd4833c2
Rolando Lopantzi <rlopantzi@gmail.com>
2020-01-06 13:22:58 -0800

    init commit

---
+++ JavaScript/js_hoisting.js
@@ -0,0 +1,117 @@
+// console.log(hello);
+// var hello = 'world';
+
+var hello = 'world';
+console.log(hello);
+
+// var needle = 'haystack';
+// test();
+// function test(){
+// 	var needle = 'magnet';
+// 	console.log(needle);
+// }
+
+var needle = 'haystack';
+function test(){
+	var needle = 'magnet';
+	console.log(needle);
+}
+test();
+
+// var brendan = 'super cool';
+// function print(){
+// 	brendan = 'only okay';
+// 	console.log(brendan);
+// }
+// console.log(brendan);
+// <div id="copy-toolbar-container" style="cursor: pointer; position: absolute; top: 5px; right: 5px; padding: 0px 3px; background: rgba(224, 224, 224, 0.2); box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 0px 0px; color: rgb(187, 187, 187); border-radius: 0.5em; font-size: 0.8em; --darkreader-inline-bgimage: initial; --darkreader-inline-bgcolor:rgba(33, 35, 37, 0.2); --darkreader-inline-boxshadow:rgba(0, 0, 0, 0.2) 0px 2px 0px 0px; --darkreader-inline-color:#c2bdb4;" data-darkreader-inline-bgimage="" data-darkreader-inline-bgcolor="" data-darkreader-inline-boxshadow="" data-darkreader-inline-color=""><span id="copy-toolbar">copy</span></div>
+
+var brendan = 'super cool';
+function print(){
+	brendan = 'only okay';
+	console.log(brendan);
+}
+console.log(brendan);
+<div id="copy-toolbar-container" style="cursor: pointer; position: absolute; top: 5px; right: 5px; padding: 0px 3px; background: rgba(224, 224, 224, 0.2); box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 0px 0px; color: rgb(187, 187, 187); border-radius: 0.5em; font-size: 0.8em; --darkreader-inline-bgimage: initial; --darkreader-inline-bgcolor:rgba(33, 35, 37, 0.2); --darkreader-inline-boxshadow:rgba(0, 0, 0, 0.2) 0px 2px 0px 0px; --darkreader-inline-color:#c2bdb4;" data-darkreader-inline-bgimage="" data-darkreader-inline-bgcolor="" data-darkreader-inline-boxshadow="" data-darkreader-inline-color=""><span id="copy-toolbar">copy</span></div>
+
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+// 	food = 'half-chicken';
+// 	console.log(food);
+// 	var food = 'gone';
+// }
+
+var food = 'chicken';
+console.log(food);
+function eat(){
+	food = 'half-chicken';
+	console.log(food);
+	var food = 'gone';
+}
+eat();
+
+// mean();
+// console.log(food);
+// var mean = function() {
+// 	food = "chicken";
+// 	console.log(food);
+// 	var food = "fish";
+// 	console.log(food);
+// }
+// console.log(food);
+
+console.log(food);
+var mean = function() {
+	food = "chicken";
+	console.log(food);
+	var food = "fish";
+	console.log(food);
+}
+console.log(food);
+mean();
+
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+// 	genre = "rock";
+// 	console.log(genre);
+// 	var genre = "r&b";
+// 	console.log(genre);
+// }
+// console.log(genre);
+
+console.log(genre);
+var genre = "disco";
+function rewind() {
+	genre = "rock";
+	console.log(genre);
+	var genre = "r&b";
+	console.log(genre);
+}
+console.log(genre);
+rewind();
+
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+// 	dojo = "seattle";
+// 	console.log(dojo);
+// 	var dojo = "burbank";
+// 	console.log(dojo);
+// }
+// console.log(dojo);
+
+dojo = "san jose";
+console.log(dojo);
+function learn() {
+	dojo = "seattle";
+	console.log(dojo);
+	var dojo = "burbank";
+	console.log(dojo);
+}
+console.log(dojo);
+learn();
